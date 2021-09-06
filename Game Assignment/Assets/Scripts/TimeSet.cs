4154afdf5f39bd68b49c0dd39f96c39a08938f55
EngrossRain <yapweichung@gmail.com>
2021-09-07 01:26:41 +0800

    current 70%

--- Game Assignment/Assets/Scripts/TimeSet.cs
+++ Game Assignment/Assets/Scripts/TimeSet.cs
@@ -20,10 +20,6 @@ public class TimeSet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Time.timeScale = 0f;
-            PauseMenu.GameIsPaused = true;
-        }
+       
     }
 }
