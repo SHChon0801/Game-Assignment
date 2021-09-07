305c474125d92414812a348d2242364dfad81130
lum9lum <lumlum.0718@gmail.com>
2021-09-07 14:19:38 +0800

    some adjustment

---
+++ Game Assignment/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    public Animator animator;
+    float moveDis = 6;
+    Vector3 localScale;
+    bool isMoveRight = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //xRight = (Screen.width - 32) / 100;
+
+        if (transform.position.x > moveDis)
+        {
+            isMoveRight = false;
+        }
+
+        if (transform.position.x < -moveDis)
+        {
+            isMoveRight = true;
+        }
+
+        if (isMoveRight)
+        {
+            moveRight();
+        }
+        else
+        {
+            moveLeft();
+        }
+
+        if (localScale.x == 1)
+        {
+            animator.SetBool("isMoveRight", true);
+        } else if(localScale.x == -1)
+        {
+            animator.SetBool("isMoveRight", false);
+        }
+        transform.Translate(new Vector3(localScale.x, 0, 0) * moveSpeed * Time.deltaTime);
+
+    }
+    void moveRight()
+    {
+        isMoveRight = true;
+        localScale.x = 1;
+        transform.localScale = localScale;        
+    }
+
+    void moveLeft()
+    {
+        isMoveRight = false;
+        localScale.x = -1; //assign the negative value for zombbie move left
+        transform.localScale = localScale;
+    }
+}
