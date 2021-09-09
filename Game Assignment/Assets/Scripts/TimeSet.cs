f09c76d0b7e3ddc5d2c73727a4773adf4fc55858
SHChon0801 <shchon1122@gmail.com>
2021-09-09 23:54:20 +0800

    idk

--- Game Assignment/Assets/Scripts/TimeSet.cs
+++ Game Assignment/Assets/Scripts/TimeSet.cs
@@ -7,20 +7,30 @@ using UnityEngine.SceneManagement;
 public class TimeSet : MonoBehaviour
 {
     [SerializeField] TimeCountDown timer1;
+    public GameObject gameOver;
+    public AudioClip gameOverSound;
+    public AudioSource audioSrc;
+
     // Start is called before the first frame update
     private void Start()
     {
         timer1.SetDuration(30)
              .Onbegin(() => Debug.Log("Time start"))
              .Onchange(s => Debug.Log("Time change" + s))
-             .Onend(() => Debug.Log("Time end"))
+             .Onend(() => gameOverS() )
              .Onpause(isPaused => Debug.Log("Pause : " + isPaused))
              .Begin();
+    }
+
+    public void gameOverS()
+    {
+        audioSrc.PlayOneShot(gameOverSound);
+        gameOver.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
     }
 }
--- Game Assignment/Assets/Scripts/TimeSet3.cs
+++ Game Assignment/Assets/Scripts/TimeSet3.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class TimeSet3 : MonoBehaviour
 {
     [SerializeField] TimeCountDown timer3;
+    public GameObject gameOver;
+    public AudioClip gameOverSound;
+    public AudioSource audioSrc;
     // Start is called before the first frame update
     private void Start()
     {
         timer3.SetDuration(90)
              .Onbegin(() => Debug.Log("Time start"))
              .Onchange(s => Debug.Log("Time change" + s))
-             .Onend(() => Debug.Log("Time end"))
+             .Onend(() => gameOverS())
              .Onpause(isPaused => Debug.Log("Pause : " + isPaused))
              .Begin();
+    }
+
+    public void gameOverS()
+    {
+        audioSrc.PlayOneShot(gameOverSound);
+        gameOver.SetActive(true);
     }
 
     // Update is called once per frame
